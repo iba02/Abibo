947e86cd33ba8fbba67312e59c700db6db4ecf7a
iba02 <154012967+iba02@users.noreply.github.com>
2024-11-03 22:35:50 +0530

    Delete backround.js

--- backround.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('Extension installed');
-});
-
-chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.create({ url: 'popup.html' });
-});
